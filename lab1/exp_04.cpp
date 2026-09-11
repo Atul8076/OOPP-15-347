f6875d2a6b265ad484aa2160e5fb8a65e5af694b
Atul8076 <atuly8076@gmail.com>
2026-09-11 09:38:43 +0530

    add lab1 folder

---
+++ lab1/exp_04.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main() {
+    // Array traversal using auto and range-based for loop
+    int arr[] = {10, 20, 30, 40, 50};
+    cout << "Array elements: ";
+    for (auto val : arr) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    
+    vector<int> vec = {1, 2, 3, 4, 5};
+    cout << "Vector elements: ";
+    for (auto &x : vec) {
+        cout << x << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
