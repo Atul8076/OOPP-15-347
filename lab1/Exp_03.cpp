b4328bf71744b48f6678f149f85cfbbbcc763f89
Atul8076 <atuly8076@gmail.com>
2026-09-25 10:25:29 +0530

    some changes

--- lab1/Exp_03.cpp
+++ lab1/Exp_03.cpp
@@ -1,40 +1,26 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class Arithmetic {
-public:
-
-
-    inline int add(int a, int b) {
-        return a + b;
+class Point{
+    int x,y;
+    public:
+    void input(){
+        cout<<"enter x : ";
+        cin>>x;
+        cout<<"enter y : ";
+        cin>>y;
     }
-
-    //  Default argument
-    int subtract(int a, int b = 5) {
-        return a - b;
-    }
-
-    // Function overloading
-    int multiply(int a, int b) {
-        return a * b;
-    }
-
-    double multiply(double a, double b) {
-        return a * b;
+    void show(){
+        cout<<"X : "<<x<<endl;
+        cout<<"Y : "<<y<<endl;
     }
 };
 
-int main() {
-
-    Arithmetic obj;
-
-    cout << "Addition " << obj.add(10, 20) << endl;
-
-    cout << "Subtraction: " << obj.subtract(20) << endl;
-
-    cout << "Multiplication: " << obj.multiply(5, 4) << endl;
-
-    cout << "Multiplication: " << obj.multiply(2.5, 4.0) << endl;
-
-    return 0;
+int main(){
+ Point p1,p2;
+ p1.input();
+ p2.input();
+ p1.show();
+ p2.show();
+ 
 }
